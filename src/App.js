1f1cd9fb028c3cca560f00c628e77f5c63455b37
jasnajais <jasnajaison7@gmail.com>
2024-12-28 23:10:18 +0530

    project setup

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import ProductDetails from './components/ProductDetails';
+import SearchProduct from './components/SearchProduct';
+import ProductList from './components/ProductList';
+
+function App() {
+  return (
+    <div>
+      <ProductDetails/><br />
+      <SearchProduct/><br />
+<ProductList/>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,142 @@
+import React from 'react'
+
+const ProductList = () => {
+  return (
+    <div>
+      <h1 align="center">PRODUCTS LIST</h1><br />
+      <div className="container">
+        <div className="row">
+          <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+
+            <div className="row g-3">
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="facewash.jpg" width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Neem Face Wash</h5>
+                    <p class="card-text"> Cleanses skin and prevents acne with the natural goodness of neem and turmeric. <br /><b>Price: ₹120</b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="lipbl.jpg" width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Lip Balm</h5>
+                    <p class="card-text">Nourishes and soothes dry, chapped lips with natural ingredients.Nourishes, soothes, protects. <br /><b>Price: ₹45</b>
+                    </p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="babysha.jpg" width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title"> Himalaya Gentle Baby Shampoo</h5>
+                    <p class="card-text"> Tear-free shampoo that cleanses and softens your baby’s hair gently. <br /><b>Price: ₹175</b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="neem.jpg" width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Purifying Neem Pack</h5>
+                    <p class="card-text">Deep cleanses pores and helps control oil and acne with neem and multani.<br /><b>Price: ₹135</b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="ashva.jpg" width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Ashwagandha Tablets</h5>
+                    <p class="card-text">  Boosts immunity and helps reduce stress with the natural power. <br /><b> Price:₹165 </b> 
+                    </p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="paste.jpg"  width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Complete Care Toothpaste</h5>
+                    <p class="card-text">Provides complete oral care with neem and pomegranate extracts.<br /><b> Price: ₹90 </b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="hair.jpg"  width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Anti-Hair Fall Shampoo</h5>
+                    <p class="card-text">Strengthens hair roots and reduces hair fall with the power of bhringraj. <br /><b>Price:₹210 </b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="eye.jpg"  width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Under Eye Cream </h5>
+                    <p class="card-text"> Reduces dark circles and puffiness with a lightweight and herbal formula for soothing<br /><b>Price: ₹200 </b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="cream.jpg"  width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Protein Hair Cream</h5>
+                    <p class="card-text">Repairs damaged hair and nourishes it with natural proteins,making it restorative.<br /><b>Price: ₹125</b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+              
+              <div className="col col-12 col-sm-6 col-md-4 col-lg-3 col-xl-3 col-xxl-3">
+
+                <div class="card">
+                  <img src="wellness.jpg"  width="300px" height="300px" class="card-img-top" alt="..."></img>
+                  <div class="card-body">
+                    <h5 class="card-title">Himalaya Wellness Chyavanprash</h5>
+                    <p class="card-text">Improves immunity and energy levels with the traditional formula of herbs. <br /><b>Price:₹350 </b></p>
+                    <a href="#" class="btn btn-primary">Buy now</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ProductList
